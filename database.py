bfca0656dd9bf7bdfa90098450ffc6baf6d897cd
Vinicius Alexandre <viniciusalexandrerodrigues7@gmail.com>
2025-07-09 03:10:40 -0300

    feat: adicionado a inserção de categoria e a inserção de dados dos produtos via dict ao banco

--- database.py
+++ database.py
@@ -47,7 +47,7 @@ def criar_tabela_produtos(conn, cursor):
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS produtos (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(100) NOT NULL,
+                nome VARCHAR(100) NOT NULL UNIQUE,
                 descricao TEXT,
                 preco DECIMAL(10, 2) NOT NULL,
                 estoque INT NOT NULL,
@@ -63,12 +63,78 @@ def criar_tabela_produtos(conn, cursor):
         conn.rollback()
         print("❌ Erro ao criar tabela:", e)
 
+def inserir_categoria(conn, cursor, nome_da_categoria):
+    try:
+
+        cursor.execute("SELECT id FROM categorias WHERE nome = %s",(nome_da_categoria,))
+        resultado = cursor.fetchone()
+        if resultado:
+            return resultado[0]
+
+        cursor.execute("""INSERT INTO categorias (nome) VALUES (%s)""", (nome_da_categoria,))
+        conn.commit()
+        return cursor.lastrowid
+
+    except Exception as e:
+        conn.rollback()
+        print(f"❌ Erro ao inserir categoria '{nome_da_categoria}':", e)
+        return None
+
+def inserir_produto(conn, cursor, tabela, dados_do_produto):
+
+    if not dados_do_produto:
+            print("❌ Nenhum dado fornecido para inserção.")
+            return None
+
+    try:
+        nome = dados_do_produto.get("nome")
+        if nome:
+            cursor.execute(
+                "SELECT id FROM produtos WHERE nome = %s", (nome,))
+            resultado = cursor.fetchone()
+            if resultado:
+                return resultado[0]
+
+        colunas = ", ".join(dados_do_produto.keys())
+        placeholders = ", ".join(["%s"] * len(dados_do_produto))
+        valores = tuple(dados_do_produto.values())
+
+        query = f"INSERT INTO {tabela} ({colunas}) VALUES ({placeholders})"
+
+        cursor.execute(query, valores)
+        conn.commit()
+        return cursor.lastrowid
+
+    except Exception as e:
+        conn.rollback()
+        print(f"❌ Erro ao inserir na tabela '{tabela}':", e)
+        return None 
+
+
+
 if __name__== "__main__":
+
+    produto1 = {
+    "nome": "Camisa Polo",
+    "descricao": "Camisa de algodão piquet de alta qualidade.",
+    "preco": 129.90,
+    "estoque": 50,
+    "categoria_id": 1
+}
 
     conn, cursor = conectar_bd()
     if conn and cursor:
-        criar_categorias = criar_tabela_categorias(conn, cursor)
-        criar_produtos = criar_tabela_produtos(conn, cursor)
+        # criar_categorias = criar_tabela_categorias(conn, cursor)
+        # criar_produtos = criar_tabela_produtos(conn, cursor)
+        id_camisa = inserir_categoria(conn, cursor, "Camisas")
+        id_bermuda = inserir_categoria(conn, cursor, "Bermudas")
+
+        print(f"ID_Camisa: {id_camisa}")
+        print(f"ID_Bermduda: {id_bermuda}")
+
+        adicionar_produto = inserir_produto(conn, cursor, "produtos", produto1)
+        print(f"ID_Camisa_Polo: {adicionar_produto}")
+
 
         cursor.close()
         conn.close()
